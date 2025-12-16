d9320d7edd797a9eb01666bacf33242f54fff5d5
komsel2228 <cuknoris@gmail.com>
2025-12-16 13:24:00 +0000

    sprint008-delete doctype airport sub terminal

--- airplane_mode/airplane_mode/doctype/airport_sub_terminal/airport_sub_terminal.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, ndk and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Airport Sub Terminal", {
-// 	refresh(frm) {
-
-// 	},
-// });
