89d275d1e1461b37b4d0aabd9f151df123824955
komsel2228 <cuknoris@gmail.com>
2025-12-16 13:12:35 +0000

    sprint005-add set query doctype airport

--- airplane_mode/airplane_mode/doctype/airport/airport.js
+++ airplane_mode/airplane_mode/doctype/airport/airport.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2025, ndk and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airport", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airport", {
+	setup(frm) {
+        frm.set_query('terminal', 'terminal_details',(doc, cdt, cdn) => {
+            return {
+                "filters": {
+                    "is_group": 1,
+                    "name": ["not in", "All Airport Terminal"]
+                }
+            };
+		});
 
-// 	},
-// });
+        frm.set_query('sub_terminal', 'terminal_details',(doc, cdt, cdn) => {
+            return {
+                "filters": {
+                    "is_group": 0
+                }
+            };
+		});
+	},
+});
