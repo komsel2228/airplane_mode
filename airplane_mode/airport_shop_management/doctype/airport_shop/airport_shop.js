f43bf3807345bf0b963314e68284c85dd6cc3451
komsel2228 <cuknoris@gmail.com>
2025-12-16 13:23:10 +0000

    sprint007-revision doctype airport shop

--- airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.js
+++ airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.js
@@ -1,8 +1,87 @@
 // Copyright (c) 2025, ndk and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airport Shop", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airport Shop", {
+	onload(frm) {
+        frappe.call({
+            method: "airplane_mode.airport_shop_management.doctype.airport_shop.airport_shop.get_terminal",
+            args:{
+                airport: frm.doc.airport || undefined
+            },
+            freeze: true,
+            freeze_message: __("Get Terminal..."),
+            callback: function(r) {
+                frm.set_query("terminal", function() {
+                    return { 
+                        filters: { 
+                            name: ["in", r.message],
+                            is_group: 1
+                    }};
+                });
+            }
+        });
 
-// 	},
-// });
+        frappe.call({
+            method: "airplane_mode.airport_shop_management.doctype.airport_shop.airport_shop.get_sub_terminal",
+            args:{
+                airport: frm.doc.airport || undefined,
+                terminal: frm.doc.terminal || undefined
+            },
+            freeze: true,
+            freeze_message: __("Get Sub Terminal..."),
+            callback: function(r) {
+                frm.set_query("sub_terminal", function() {
+                    return { 
+                        filters: {
+                            parent_airport_terminal: frm.doc.terminal,
+                            name: ["in", r.message],
+                            is_group: 0
+                    }};
+                });
+            }
+        });
+	},
+    airport(frm) {
+        frm.set_value("terminal",undefined)
+        frm.set_value("sub_terminal",undefined)
+        frappe.call({
+            method: "airplane_mode.airport_shop_management.doctype.airport_shop.airport_shop.get_terminal",
+            args:{
+                airport: frm.doc.airport
+            },
+            freeze: true,
+            freeze_message: __("Get Terminal..."),
+            callback: function(r) {
+                frm.set_query("terminal", function() {
+                    return { 
+                        filters: { 
+                            name: ["in", r.message],
+                            is_group: 1
+                    }};
+                });
+            }
+        });
+	},
+    terminal(frm) {
+        frm.set_value("sub_terminal",undefined)
+        frappe.call({
+            method: "airplane_mode.airport_shop_management.doctype.airport_shop.airport_shop.get_sub_terminal",
+            args:{
+                airport: frm.doc.airport,
+                terminal: frm.doc.terminal
+            },
+            freeze: true,
+            freeze_message: __("Get Sub Terminal..."),
+            callback: function(r) {
+                frm.set_query("sub_terminal", function() {
+                    return { 
+                        filters: {
+                            parent_airport_terminal: frm.doc.terminal,
+                            name: ["in", r.message],
+                            is_group: 0
+                    }};
+                });
+            }
+        });
+	},
+});
--- airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.py
+++ airplane_mode/airport_shop_management/doctype/airport_shop/airport_shop.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, ndk and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AirportShop(Document):
 	pass
+
+@frappe.whitelist()
+def get_terminal(airport=None):
+	get_terminal = frappe.db.sql("""select terminal from `tabAirport Detail Terminal` where parent = %s""",airport,as_dict=1)
+	arr = []
+	for i in get_terminal:
+		arr.append(i.terminal)
+	
+	return arr
+
+@frappe.whitelist()
+def get_sub_terminal(airport=None,terminal=None):
+	get_sub_terminal = frappe.db.sql("""select sub_terminal from `tabAirport Detail Terminal` where parent = %s and terminal = %s""",(airport,terminal),as_dict=1)
+	arr = []
+	for i in get_sub_terminal:
+		arr.append(i.sub_terminal)
+	
+	return arr
+
